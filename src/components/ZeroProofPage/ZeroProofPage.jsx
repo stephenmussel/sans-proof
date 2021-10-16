114833c442f31baea902ee86529c69bbd9c34b6d
phaydara <phaydara@gmail.com>
2021-10-16 17:37:42 -0500

    fetchBusiness saga wired

--- src/components/ZeroProofPage/ZeroProofPage.jsx
+++ src/components/ZeroProofPage/ZeroProofPage.jsx
@@ -1,3 +1,4 @@
+import { use } from 'passport';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -5,6 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 function ZeroProofPage() {
 
   const businessList = useSelector(store => store.business)
+  const dispatch = useDispatch();
+
+  // GETs all businesses on page load
+  useEffect(() => {
+    const action = {type: 'FETCH_BUSINESS'}
+    dispatch(action)
+  }, [])
 
   return (
     <div className="container">
