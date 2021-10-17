2625e5e21acd3488308c07f21231cc15ed735488
phaydara <phaydara@gmail.com>
2021-10-17 12:46:50 -0500

    postBusiness saga wired

--- src/redux/sagas/business.saga.js
+++ src/redux/sagas/business.saga.js
@@ -3,21 +3,27 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* fetchBusiness() {
     try {
-        yield console.log('fetchBusiness saga wired!');
+        // yield console.log('fetchBusiness saga wired!');
         const response = yield axios.get('/api/business');
-        yield console.log('this is response from DB', response.data);
+        // yield console.log('this is response from DB', response.data);
         yield put({type: 'SET_BUSINESS', payload: response.data})
-        
-        
-
     } catch(error) {
-        console.log('error in fetchBusiness saga', error);
+        console.log('error in fetchBusiness saga', error); 
+    }
+}
+
+function* postBusiness() {
+    try {
+        yield console.log('postBusiness saga wired!');
         
+    } catch(error) {
+        console.log('error in postBusiness', error);
     }
 }
 
 function* businessSaga() {
     yield takeLatest('FETCH_BUSINESS', fetchBusiness);
+    yield takeLatest('POST_BUSINESS', postBusiness);
 }
 
 export default businessSaga;
