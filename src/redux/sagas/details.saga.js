17d737d78778365fe00de9ff9c2ac904cd0e3aee
phaydara <phaydara@gmail.com>
2021-10-17 11:21:57 -0500

    dispatched data to details reducer and displays on DOM

--- src/redux/sagas/details.saga.js
+++ src/redux/sagas/details.saga.js
@@ -3,9 +3,13 @@ import { put, takeLatest } from 'redux-saga/effects';
 
 function* fetchSelectedDetails(action) {
     try {
-        yield console.log('fetchSelectedDetails saga wired!');
-        
-
+        // yield console.log('fetchSelectedDetails saga wired!');
+        // yield console.log('this is selected business ID ', action.payload);
+        const businessId = action.payload;
+        const selectedDetails = yield axios.get(`/api/business/${businessId.id}`);
+        // console.log('this is selectedDetails: ', selectedDetails.data);
+        // "dispatches" to details reducer
+        yield put({type: 'SET_DETAILS', payload: selectedDetails.data})
     } catch(error) {
         console.log('error in fetchSelectedDetails', error);
         
