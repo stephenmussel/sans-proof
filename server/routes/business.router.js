15bf4f9c128f4b51ce58c7d1460b41a432ae5767
phaydara <phaydara@gmail.com>
2021-10-17 11:00:39 -0500

    GET selected details in business router

--- server/routes/business.router.js
+++ server/routes/business.router.js
@@ -22,7 +22,18 @@ router.get('/', rejectUnauthenticated, (req, res) => {
  */
 router.get('/:id', (req, res) => {
   console.log('in GET details');
-  
+  console.log('this is req.params: ', req.params);
+  const businessId = req.params.id;
+  console.log('this is businessId: ', businessId);
+  const detailQuery = `SELECT * FROM "business" WHERE "business"."id" = $1;`;
+  pool.query(detailQuery, [businessId])
+    .then(result => {
+      console.log('this is result: ', result);
+      res.send(result.rows[0]);
+    }).catch(error => {
+      console.log('error in GET details', error);
+      res.sendStatus(500);
+    })
 })
 
 
