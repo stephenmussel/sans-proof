2af676b51b24d28350c074f079f451280eed3ee2
phaydara <phaydara@gmail.com>
2021-10-17 23:05:31 -0500

    fetchFav saga wired

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProfilePage() {
 
-    const user = useSelector(store => store.user)
+    const user = useSelector(store => store.user);
+    const favoriteList = useSelector(store => store.favorite);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const action = {type: 'FETCH_FAVORITE'}
+        dispatch(action)
+    }, [])
 
     return(
         <div>
             <h3>{user.username}'s Profile</h3>
+            {JSON.stringify(favoriteList)}
         </div>
     )
 }
--- src/redux/sagas/favorite.saga.js
+++ src/redux/sagas/favorite.saga.js
@@ -9,13 +9,24 @@ function* postFav(action) {
         yield axios.post(`/api/favorite/${favId}`);
         // will need to GET when displaying fav in profile
     } catch(error) {
-        console.log('error in adding favorite', error);
+        console.log('error in adding favorite', error);  
+    }
+}
+
+function* fetchFav() {
+    try {
+        yield console.log('fetchFav wired!');
+        
+
+    } catch(error) {
+        console.log('error if fetchFav', error);
         
     }
 }
 
 function* favoriteSaga() {
-    yield takeLatest('POST_FAVORITE', postFav)
+    yield takeLatest('POST_FAVORITE', postFav);
+    yield takeLatest('FETCH_FAVORITE', fetchFav);
 }
 
 export default favoriteSaga;
